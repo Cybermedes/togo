84a67d7e290eef6ed8e3e0bde781799d51512636
Lucas Paiva <lpaivamatiazzi@gmail.com>
2024-05-28 17:46:11 -0300

    feat: add basic cli flags to call funcs

--- main.go
+++ main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+const todoFile = ".saved_todos.json"
+
+func errorHandling(err error) {
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+}
 
 func main() {
-	fmt.Println("Hello World!")
+
+	add := flag.String("add", "", "add a new todo")
+	complete := flag.Int("complete", 0, "mark a todo as completed")
+	delete := flag.Int("delete", 0, "delete a todo from the table")
+
+	flag.Parse()
+
+	todos := &Todos{}
+
+	err := todos.LoadFromFile(todoFile)
+	errorHandling(err)
+
+	switch {
+
+	case *add != "":
+		todos.Add(*add)
+		err := todos.SaveToFile(todoFile)
+		errorHandling(err)
+
+	case *complete > 0:
+		err = todos.Complete(*complete)
+		errorHandling(err)
+		err = todos.SaveToFile(todoFile)
+		errorHandling(err)
+
+	case *delete > 0:
+		err = todos.Delete(*delete)
+		errorHandling(err)
+		err = todos.SaveToFile(todoFile)
+		errorHandling(err)
+
+	default:
+		fmt.Fprintln(os.Stderr, "invalid command")
+		os.Exit(0)
+	}
 }
